b27873e9e0ffe4aae15eb392ccfe2b81f1911914
Christoph Heim <post@christoph-heim.de>
2022-10-05 19:41:04 +0200

    fixed image quality bug & Cube CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,97 +1,123 @@
 const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
-const Image = require('@11ty/eleventy-img')
 const path = require('path')
+const Image = require('@11ty/eleventy-img')
 
 // IMAGE SHORTCODE
 async function imageShortcode(src, alt, sizes) {
-	const options = {
+  const options = {
+    useCache: true, // set to false while you are playing with the img quality
+    widths: [300, 600, 1200],
+    formats: ['avif', 'webp', 'jpg'],
+    outputDir: 'dist/img',
+    sharpWebpOptions: {
+    	quality: 85,
+    	smartSubsample: true,
+    	effort: 6
+    },
+    sharpJpegOptions: {
+    	quality: 85
+    },
+    sharpAvifOptions: {
+    	quality: 85
+    },
 
-		widths: [300, 600, 1200],  //
-		formats: ['avif', 'webp', 'jpg'],
-		outputDir: 'dist/img',
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src)
+      const name = path.basename(src, extension)
 
-		filenameFormat: function (id, src, width, format, options) {
-			const extension = path.extname(src)
-			const name = path.basename(src, extension)
+      return `${name}-${width}w.${format}`
+    }
+  }
 
-			return `${name}-${width}w.${format}`
-		}
-	}
+  const metadata = await Image(src, options)
 
-	const metadata = await Image(src, options)
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: 'lazy',
+    decoding: 'async'
+  }
 
-	let imageAttributes = {
-		alt,
-		sizes,
-		loading: 'lazy',
-		decoding: 'async'
-	}
-
-	return Image.generateHTML(metadata, imageAttributes, {
-		whitespaceMode: 'inline'
-	})
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: 'inline'
+  })
 }
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(EleventyVitePlugin, {
-
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
     viteOptions: {
       clearScreen: false,
+
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `$injectedColor: orange;`,
+            devSourcemap: true
+          }
+        }
+      },
+
       server: {
-        mode: "development",
-        middlewareMode: true,
+        mode: 'development',
+        middlewareMode: true
       },
       appType: 'custom',
       build: {
-        mode: "production",
+        mode: 'production',
         rollupOptions: {
           output: {
             chunkFileNames: 'js/[name]-[hash].js',
             entryFileNames: 'js/[name]-[hash].js',
 
-            assetFileNames: ({name}) => {
-              if (/\.(avif|webp|gif|jpe?g|png|svg)$/.test(name ?? '')){
-                  return 'img/[name]-[hash][extname]';
+            assetFileNames: ({ name }) => {
+              if (/\.(avif|webp|gif|jpe?g|png|svg)$/.test(name ?? '')) {
+                return 'img/[name]-[hash][extname]'
+              }
+
+              if (/\.(woff|woff2|otf)$/.test(name ?? '')) {
+                return 'fonts/[name]-[hash][extname]'
               }
 
               if (/\.css$/.test(name ?? '')) {
-                  return 'css/[name]-[hash][extname]';
+                return 'css/[name]-[hash][extname]'
               }
-
 
               // default value
               // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-              return '[name]-[hash][extname]';
-            },
-          },
+              return '[name]-[hash][extname]'
+            }
+          }
         }
       }
     }
-	})
+  })
 
-	// Passthrough copy
-	eleventyConfig.addPassthroughCopy('src/css')
-	eleventyConfig.addPassthroughCopy('src/js')
-	eleventyConfig.addPassthroughCopy('src/img')
-	eleventyConfig.addPassthroughCopy({ 'src/public': 'public' })
-	eleventyConfig.setServerPassthroughCopyBehavior('copy')
+  eleventyConfig.setServerOptions({
+  showAllHosts:true,}),
 
-	// image shortcodes
-	eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode)
-	eleventyConfig.addLiquidShortcode('image', imageShortcode)
-	eleventyConfig.addJavaScriptFunction('image', imageShortcode)
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy('src/css')
+  eleventyConfig.addPassthroughCopy('src/js')
+  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy('src/public')
+  eleventyConfig.setServerPassthroughCopyBehavior('copy')
 
-	return {
-		templateFormats: ['md', 'njk', 'html', 'liquid'],
-		dataTemplateEngine: 'njk',
-		htmlTemplateEngine: 'njk',
+  // image shortcodes
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode)
+  eleventyConfig.addLiquidShortcode('image', imageShortcode)
+  eleventyConfig.addJavaScriptFunction('image', imageShortcode)
 
-		dir: {
-			input: 'src',
-			output: 'dist',
-			includes: '_includes',
-			layouts: '_layouts',
-			data: '_data'
-		}
-	}
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+
+    dir: {
+      input: 'src',
+      output: 'dist',
+      includes: '_includes',
+      layouts: '_layouts',
+      data: '_data'
+    }
+  }
 }
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,17 @@
-module.exports = {
-  plugins: [
-    require('postcss-preset-env'),
-    require('autoprefixer'),
-    require('postcss-import'),
-    require('postcss-nested'),
-    // require('postcss-mixins'), @mixin
-    // require('postcss-atroot'), @at-root
-    // require('postcss-conditionals'), //@if
-    // require('postcss-for'), @for
-    // require('postcss-each'), @each
-    // require('postcss-short')
-    // require('postcss-apply'),
-    // require('postcss-font-magician')
-  ]
-}
+// module.exports = {
+//   map: 'inline',
+//   plugins: [
+//     require('postcss-preset-env'),
+//     require('autoprefixer'),
+//     require('postcss-import'),
+//     require('postcss-nested'),
+//     // require('postcss-mixins'), @mixin
+//     // require('postcss-atroot'), @at-root
+//     // require('postcss-conditionals'), //@if
+//     // require('postcss-for'), @for
+//     // require('postcss-each'), @each
+//     // require('postcss-short')
+//     // require('postcss-apply'),
+//     // require('postcss-font-magician')
+//   ]
+// }
